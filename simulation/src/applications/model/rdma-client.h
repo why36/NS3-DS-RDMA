3e3d05fba034f3ffd3f8bfe03d8bce0d07f55548
Krayecho <532820040@qq.com>
2020-07-15 14:39:20 +0800

    format

--- simulation/src/applications/model/rdma-client.h
+++ simulation/src/applications/model/rdma-client.h
@@ -23,11 +23,12 @@
 #ifndef RDMA_CLIENT_H
 #define RDMA_CLIENT_H
 
+#include <ns3/rdma.h>
+
 #include "ns3/application.h"
 #include "ns3/event-id.h"
-#include "ns3/ptr.h"
 #include "ns3/ipv4-address.h"
-#include <ns3/rdma.h>
+#include "ns3/ptr.h"
 
 namespace ns3 {
 
@@ -40,44 +41,41 @@ class Packet;
  * \brief A RDMA client.
  *
  */
-class RdmaClient : public Application
-{
-public:
-  static TypeId
-  GetTypeId (void);
+class RdmaClient : public Application {
+   public:
+    static TypeId GetTypeId(void);
 
-  RdmaClient ();
+    RdmaClient();
 
-  virtual ~RdmaClient ();
+    virtual ~RdmaClient();
 
-  /**
-   * \brief set the remote address and port
-   * \param ip remote IP address
-   * \param port remote port
-   */
-  void SetRemote (Ipv4Address ip, uint16_t port);
-  void SetLocal (Ipv4Address ip, uint16_t port);
-  void SetPG (uint16_t pg);
-  void SetSize(uint64_t size);
-  void Finish();
+    /**
+     * \brief set the remote address and port
+     * \param ip remote IP address
+     * \param port remote port
+     */
+    void SetRemote(Ipv4Address ip, uint16_t port);
+    void SetLocal(Ipv4Address ip, uint16_t port);
+    void SetPG(uint16_t pg);
+    void SetSize(uint64_t size);
+    void Finish();
 
-protected:
-  virtual void DoDispose (void);
+   protected:
+    virtual void DoDispose(void);
 
-private:
+   private:
+    virtual void StartApplication(void);
+    virtual void StopApplication(void);
 
-  virtual void StartApplication (void);
-  virtual void StopApplication (void);
+    uint64_t m_size;
+    uint16_t m_pg;
 
-  uint64_t m_size;
-  uint16_t m_pg;
-
-  Ipv4Address m_sip, m_dip;
-  uint16_t m_sport, m_dport;
-  uint32_t m_win; // bound of on-the-fly packets
-  uint64_t m_baseRtt; // base Rtt
+    Ipv4Address m_sip, m_dip;
+    uint16_t m_sport, m_dport;
+    uint32_t m_win;      // bound of on-the-fly packets
+    uint64_t m_baseRtt;  // base Rtt
 };
 
-} // namespace ns3
+}  // namespace ns3
 
 #endif /* RDMA_CLIENT_H */
